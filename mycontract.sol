9b82ec9a137e4ed5af8a7fc6014e4e9f7b8d88c2
EasChen70 <eason_chen@uri.edu>
2024-08-29 22:38:57 -0400

    Imported IERC20 code

--- mycontract.sol
+++ mycontract.sol
@@ -1,3 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
+import "./IERC20.sol";
+contract Vault{
+    IERC20 public immutable token;
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
 
+    constructor(address _token){
+        token = IERC20(_token);
+    }
+
+}
