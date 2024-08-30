68be87a7decf3d6a3c4327d2cb2d1ef445aa34b2
EasChen70 <eason_chen@uri.edu>
2024-08-30 16:03:23 -0400

    deposit + withdraw functions

--- mycontract.sol
+++ mycontract.sol
@@ -10,18 +10,33 @@ contract Vault{
         token = IERC20(_token);
     }
 
-    function mint(address _to, uint _amount) private{
+    function _mint(address _to, uint _amount) private{
         totalSupply += _amount;
         balanceOf[_to] += _amount;
     }
 
-    function burn(address _from, uint _amount) private{
+    function _burn(address _from, uint _amount) private{
         totalSupply -= _amount;
         balanceOf[_from] -= _amount;
     }
 
     function deposit(uint _amount) external{
-        
+        uint shares;
+        if (totalSupply == 0){
+            shares = _amount;
+        }
+        else{
+            shares = _amount*totalSupply / token.balanceOf(address(this));
+        }
+        _mint(msg.sender, shares);
+        token.transferFrom(msg.sender, address(this), _amount);
     }
 
+    function withdraw(uint _shares) external{
+        uint amount = (_shares*token.balanceOf(address(this)))/ totalSupply;
+        _burn(msg.sender, _shares);
+        token.transfer(msg.sender, amount);
+    }
+
+    
 }
